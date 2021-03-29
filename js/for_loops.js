201200fa575ab69608e7dbc2f9a90c30ca10a32f
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2021-03-29 12:32:17 -0500

    completed for loops exercises

---
+++ js/for_loops.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// var number = 7;
+
+// console.log(number + " * 1 = " + number *1);
+
+// for(var multiplier = 1; multiplier <= 10; multiplier++) {
+//     console.log(number + " * " + multiplier + " = " + number * multiplier);
+// }
+
+// function showMultiplicationTable(number){
+//     for(var multiplier = 1; multiplier <= 10; multiplier++) {
+//         console.log(number + " * " + multiplier + " = " + number * multiplier);
+//     }
+// }
+//
+// showMultiplicationTable(7);
+
+// for(var i = 0; i < 10; i++){
+//     var randomNum = Math.floor(Math.random() * 181) + 20;
+//     if(randomNum % 2 === 0){
+//         console.log(randomNum + " is even!");
+//     } else {
+//         console.log(randomNum + " is odd!");
+//     }
+// }
+
+// for(var i = 1; i <= 9; i++) {
+//     var clone = "";
+//     for(var o = 0; o < i; o++) {
+//         clone += i;
+//     }
+//     console.log(clone);
+// }
+
+// for(var i = 100; i > 0; i = i - 5) {
+//     console.log(i);
+// }
