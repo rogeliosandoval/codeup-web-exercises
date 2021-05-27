99ef1b9514c730b9fd489a734548e59291c30457
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2021-05-26 22:54:08 -0500

    push

---
+++ js/warmup.js
@@ -0,0 +1,39 @@
+// function fizzBuzz(num) {
+//     for(var i = 1; i <= num; i++) {
+//         if( i % 15 === 0) {
+//             console.log("fizzbuzz")
+//         }
+//         else if(i % 15 === 0) {
+//             console.log("fizz")
+//         }
+//         else if(i % 3 === 0) {
+//             console.log("buzz")
+//         }
+//         else {
+//             console.log(i);
+//         }
+//     }
+// }
+//
+// fizzBuzz(30)
+//
+// console.log(fizzBuzz());
+//
+// function addTo7(num) {
+//     num = 5 + 2
+//     return num;
+// }
+//
+// console.log(addTo7());
+
+function takesInANumberAndReturnsThePastNumberMultipliedByFive (num) {
+    if (typeof num === "number") {
+        return num * 5;
+    }
+    return 0
+}
+
+console.log(takesInANumberAndReturnsThePastNumberMultipliedByFive(3))
+
+
+
